13d028573394a019a2257aad47e3bb4ceb2b6f41
Hack Reactor Students <accounts@hackreactor.com>
2016-06-30 12:14:04 -0700

    Parse URL and check directory for URL

--- helpers/archive-helpers.js
+++ helpers/archive-helpers.js
@@ -1,31 +1,32 @@
 var fs = require('fs');
 var path = require('path');
 var _ = require('underscore');
+var redis = require('redis');
+var client = redis.createClient();
+var url = require('url');
 
-/*
- * You will need to reuse the same paths many times over in the course of this sprint.
- * Consider using the `paths` object below to store frequently used file paths. This way,
- * if you move any files, you'll only need to change your code in one place! Feel free to
- * customize it in any way you wish.
- */
 
-exports.paths = {
-  siteAssets: path.join(__dirname, '../web/public'),
-  archivedSites: path.join(__dirname, '../archives/sites'),
-  list: path.join(__dirname, '../archives/sites.txt')
+exports.parseURl = function(inputUrl) {
+  var urlObj = url.parse(inputUrl);
+  inputUrl = !urlObj.protocol ? 'http://' + inputUrl : inputUrl;
+  urlObj = url.parse(inputUrl);
+  return !urlObj.hostname ? false : urlObj.hostname + urlObj.pathname;
 };
 
-// Used for stubbing paths for tests, do not modify
-exports.initialize = function(pathsObj) {
-  _.each(pathsObj, function(path, type) {
-    exports.paths[type] = path;
+exports.isInDirectoryAsync = function(inputUrl) {
+  return new Promise(function(resolve, reject) {
+    client.hexists('directory', inputUrl, function(err, result) {
+      result === 1 ? resolve(inputUrl) : reject(err);
+    });    
   });
 };
 
-// The following function names are provided to you to suggest how you might
-// modularize your code. Keep it clean!
-
-exports.readListOfUrls = function() {
+exports.getSiteVersionsAsync = function(inputUrl) {
+  return new Promise(function(resolve, reject) {
+    client.hget('directory', inputUrl, function(err, result) {
+      !err ? resolve(JSON.parse(result)) : reject(err);
+    });
+  });
 };
 
 exports.isUrlInList = function() {
