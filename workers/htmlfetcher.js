910ca0c57c5f8142552c06238012d71d8942390e
Hack Reactor Students <accounts@hackreactor.com>
2016-06-30 19:55:40 -0700

    Store each directory in the queue.

--- workers/htmlfetcher.js
+++ workers/htmlfetcher.js
@@ -3,26 +3,25 @@
 var http = require('../helpers/http-helpers');
 var archive = require('../helpers/archive-helpers');
 var request = require('request');
-
-archive.pushToQueue('www.metavision.com/');
-var inputUrl, versionUrl, timestamp;
-archive.popUrl().then(function(url) {
-  inputUrl = url;
-  timestamp = Date.now();
-  versionUrl = inputUrl + timestamp;
-  return http.downloadSite(inputUrl);
-})
-.then(function(body) {
-  return archive.storeSite(versionUrl, body);
-})
-.then(function() {
-  return archive.updateDirectory(inputUrl, timestamp);
-})
-.catch(function(err) {
-  console.log('err ', err);
+archive.popQueue().then(function(queue) {
+  var worked = [];
+  queue.forEach(function(inputUrl) {
+    // If we haven't already seen this site
+    // this queue pop.
+    if (worked.indexOf(inputUrl) === -1) {
+      worked.push(inputUrl);
+      var timestamp = Date.now();
+      var versionUrl = inputUrl + timestamp;
+      http.downloadSite(inputUrl)
+      .then(function(body) {
+        return archive.storeSite(versionUrl, body);
+      })
+      .then(function() {
+        return archive.updateDirectory(inputUrl, timestamp);
+      })
+      .catch(function(err) {
+        console.log('err ', err);
+      });
+    }
+  });
 });
-
-//   archive.storeSite(versionUrl, data);
-// }).catch(function(err) {
-//   console.log(err);
-// });
