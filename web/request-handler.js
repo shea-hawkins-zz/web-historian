97e5ce76ce318611b68339f5f17658c125445ef3
Hack Reactor Students <accounts@hackreactor.com>
2016-06-30 17:20:05 -0700

    Serve pages already in storage.

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,23 +1,39 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
 var http = require('../helpers/http-helpers');
+var qs = require('querystring');
 // require more modules/folders here!
+
+
 
 exports.handleRequest = function (req, res) {
   if (req.method === 'POST') {
-    console.log(http.getDataAsync(req).then(function(data) {
-      console.log(data);
-    }));
-
-
-    // exports.isInDirectory('www.google.com')
-    // .then(function() {
-    //   return exports.getSiteVersions('www.google.com');
-    // })
-    // .then(function(versions) {
-    //   versions[versions.length - 1];
-    // });
-
+    var inputUrl, versionUrl;
+    http.getData(req).then(function(data) {
+      var form = qs.parse(data);
+      inputUrl = archive.parseUrl(form.url);
+      return archive.isInDirectory(inputUrl);
+    })
+    .then(function(inputUrl) {
+      return archive.getSiteVersions(inputUrl);
+    })
+    .then(function(versions) {
+      versionUrl = inputUrl + versions[versions.length - 1];
+    })
+    .then(function() {
+      return archive.loadSite(versionUrl);
+    })
+    .then(function(html) {
+      http.serveAssets(res, html);
+    })
+    .catch(function(err) {
+      console.log('adding ' + inputUrl + ' to queue');
+      archive.pushToQueue(inputUrl);
+      http.serveFile(res, path.join(__dirname + '/public/loading.html'));
+    });
+  } else if (req.method === 'GET') {
+    http.serveFile(res, path.join(__dirname + '/public/index.html'));
   }
-
 };
+ 
+
